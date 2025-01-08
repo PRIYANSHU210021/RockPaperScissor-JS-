76e18f88f56122c4d87fe9b935d5c0632599b3f3
Priyanshu Prajapati <priyanshu210021@gmail.com>
2025-01-08 18:33:23 +0530

    SPS Commited 01

---
+++ script.js
@@ -0,0 +1,48 @@
+const btns = document.querySelectorAll('button');
+const res = document.getElementById("result");
+const userScore = document.getElementById("user-score")
+const compScore = document.getElementById("comp-score")
+
+let userScr = 0;
+let compScr = 0;
+
+btns.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        // console.log(btn.id)
+        const result = gameplay(btn.id,compChoice())
+        res.innerHTML = result;
+    })
+})
+
+
+const compChoice = ()=>{
+    // or hmm ye bhi kar skte hai 3 likhne ke bajaye
+    const choices = ["Rock","Paper","Scissor"];
+    // Math.floor(Math.random(choices.length));
+
+    const randomChoice = Math.floor(Math.random()*3);
+    return choices[randomChoice];
+}
+
+const gameplay = (user,comp)=>{
+    if(user==comp) return "Match tie";
+    else if( 
+        (user=="Rock" && comp == "Scissor") ||
+        (user=="Paper" && comp == "Rock") ||
+        (user=="Scissor" && comp == "Paper")
+    )
+    {
+        
+        userScr++;
+        userScore.innerHTML = userScr;
+        return `Tu Jit Gaila Ho Bhaiya! <br>${user} beats ${comp}`
+    }
+    else
+    {
+        compScr++;
+        compScore.innerHTML = compScr;
+        return `Bhkk Marde Computerwa Se Har Gaila! <br><span style="color: red;">${user} beats ${comp}</span>`
+    }
+
+}
+
